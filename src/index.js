5a643945dd160f4b672d195095d1066d0c5766a5
mery <merryoff@gmail.com>
2021-03-13 14:00:13 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length == 0 || (array.length === 0)) {
+        return 0
+    }
+    return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length == 0 || (array.length === 0)) {
+        return 0
+    }
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length == 0 || (array.length === 0)) {
+        return 0
+    }
+    var c = 0;
+    for (i = 0; i < array.length; i++) {
+        c += array[i]
+    }
+    return c / array.length;
 }
